5599836e5fb784f171010e19fb3a0ff9c12f3fa1
open222333 <open222333@gmail.com>
2021-05-14 18:33:36 +0800

    20210514-183336

--- LeetCode/0003_LongestSubstringWithoutRepeatingCharacters.py
+++ LeetCode/0003_LongestSubstringWithoutRepeatingCharacters.py
@@ -9,15 +9,19 @@ class Solution:
         left_pointer = 0  # 左邊指針從0開始
         output = 0  # 窗口大小
         for right_pointer in range(len(s)):
+            # 如果右指針的字元不在字典內
             if s[right_pointer] not in seen:
-                #  如果右指針的字元不在字典內，取output跟目前窗口大小(r-l+1)的最大值
+                # 取output跟目前窗口大小(r-l+1)的最大值
                 output = max(output, right_pointer - left_pointer + 1)
+            # 如果右指針的字元在字典內
             else:
                 if seen[s[right_pointer]] < left_pointer:
+                    # 取output跟目前窗口大小(r-l+1)的最大值
                     output = max(output, right_pointer - left_pointer + 1)
+                # 若有相同字元且右指針字元的索引值大於左指針
                 else:
-                    # 若有相同字元且右指針字元的索引值大於左指針
                     # 則左指針移動到目前右指針的右邊一位
                     left_pointer = seen[s[right_pointer]] + 1
+            # 儲存{字母：索引值} seen[charactor] = index
             seen[s[right_pointer]] = right_pointer
         return output
