539ae8c01a304ced150364a63de759a92942a365
open222333 <open222333@gmail.com>
2021-05-24 10:12:25 +0800

    20210524-101225

--- LeetCode/0005_LongestPalindromicSubstring.py
+++ LeetCode/0005_LongestPalindromicSubstring.py
@@ -5,6 +5,29 @@
 # Approach 4: Expand Around Center
 # Approach 5: Manacher's Algorithm
 
+class Solution5():
+    # Manacher algorithm
+    # http://en.wikipedia.org/wiki/Longest_palindromic_substring
+
+    def longestPalindrome(self, s):
+        transString = '#'.join('^{}$'.format(s))  # 在字串的字元間加上# ^$是邊界
+        # S = "abba", T = "^#a#b#b#a#$"
+        n = len(transString)
+        palindromeL = [0] * n
+        centerP = rightP = 0
+        for i in range(1, n - 1):
+            palindromeL[i] = (rightP > i) and min(
+                rightP - i, palindromeL[2 * centerP - i])
+            # 以i為中心向外展開
+            while transString[i + 1 + palindromeL[i]] == \
+                    transString[i - 1 - palindromeL[i]]:
+                palindromeL[i] += 1
+            if i + palindromeL[i] > rightP:
+                centerP, rightP = i, i + palindromeL[i]
+
+        maxLen, centerIndex = max((n, i) for i, n in enumerate(palindromeL))
+        return s[(centerIndex - maxLen) // 2: (centerIndex + maxLen) // 2]
+
 
 class Solution_OldCodingFarmer():
     def longestPalindrome(self, s):
@@ -18,8 +41,9 @@ class Solution_OldCodingFarmer():
     # get the longest palindrome, l, r are the middle indexes
     # from inner to outer
 
-    def helper(self, s, l_index, r):
-        while 0 <= l_index and r < len(s) and s[l_index] == s[r]:
-            l_index -= 1
-            r += 1
-        return s[l_index + 1: r]
+    def helper(self, s, left, right):
+        # right left 由內到外 比對是否為相同字元
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return s[left + 1: right]
