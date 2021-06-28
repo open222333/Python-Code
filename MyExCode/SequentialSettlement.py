bb085f3211b01ebb9fec790bb9e080b2570b1751
TomLi <open222333@gmail.com>
2021-06-28 11:39:34 +0800

    20210628-113933

--- MyExCode/SequentialSettlement.py
+++ MyExCode/SequentialSettlement.py
@@ -1,35 +1,48 @@
-class SequentialSettlement():
-    def __init__(self, *money, **bet) -> None:
-        self.betmakerMoneyList = []
-        self.betList = []
-        for i in money:
-            self.betmakerMoneyList.append(i)
-        for j in bet:
-            self.betList.append(j)
+class SettlementScheme():
+    def __init__(self, money: list, bet: list) -> None:
+        self.betMakerMoneyList = money
+        self.betAmountList = bet
 
-    def getBetMakerMoney(self):
-        return self.betmakerMoneyList
+    def SequentialSettlement(self):
+        for betAmount in self.betAmountList:
+            a = 0
+            totalMoney = 0
+            while True:
+                totalMoney += self.betMakerMoneyList[a]
+                if totalMoney < betAmount:
+                    self.betMakerMoneyList[a] -= self.betMakerMoneyList[a]
+                else:
+                    amount = totalMoney - betAmount
+                    settleAmount = self.betMakerMoneyList[a] - amount
+                    self.betMakerMoneyList[a] = amount
+                    break
+                a += 1
+        return
 
-    def getBetList(self):
-        return self.betList
 
+betMaker = [10000, 5000, 1000000000]
+betmoneylist = [[6000, 6000, 1000, 1500, 10000, 10000], [
+    6000, 6000, 1000, 1500, 10000, 10000], [6000, 6000, 1000, 1500, 10000, 10000]]
 
-betMaker = SequentialSettlement(10000, 10000, 1000000).getBetMakerMoney()
-betList = SequentialSettlement(10000, 10000, 1000000,[12000, 1700, 1000, 1500]).getBetList()
 print(betMaker)
-print(betList)
-betmoneylist = [12000, 1700, 1000, 1500]
-
 for betmoney in betmoneylist:
     a = 0
     totalMoney = 0
+    settleCount = []
     while True:
         totalMoney += betMaker[a]
         if totalMoney < betmoney:
+            settleCount.append(betMaker[a])
             betMaker[a] -= betMaker[a]
         else:
-            amount = totalMoney - betmoney
-            settleAmount = betMaker[a] - amount
-            betMaker[a] = amount
+            BMamount = totalMoney - betmoney
+            settleAmount = betMaker[a] - BMamount
+            betMaker[a] = BMamount
+            settleCount.append(settleAmount)
+            bm = 1
+            for s in settleCount:
+                if s is not 0:
+                    print(f"第{bm}位，賠{s}")
+                bm += 1
             break
         a += 1
--- test.py
+++ test.py
@@ -1,3 +1,14 @@
-import uuid
+a1 = ['a', 'b', 'c']
+a2 = ['d', 'e', 'f']
+a3 = []
+a3 = a1
 
-print(uuid.uuid4())
+def test(l1,l2):
+    ans = []
+    for i in l1:
+        for j in l2:
+            ans.append(i + j)
+    return ans
+
+print(test(a1, a2))
+print(a3)
