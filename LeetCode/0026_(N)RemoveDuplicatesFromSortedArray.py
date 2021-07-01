19eceb772b0fa1020e51634a97cd00344adf11d9
TomLi <open222333@gmail.com>
2021-07-01 12:53:44 +0800

    20210701-125344

--- LeetCode/0026_(N)RemoveDuplicatesFromSortedArray.py
+++ LeetCode/0026_(N)RemoveDuplicatesFromSortedArray.py
@@ -1,30 +1,43 @@
 # Approach 1: Two Pointers
 class Solution:
     def removeDuplicates(self, nums: list[int]) -> int:
-        stack = []
-        result = []
-        length = 0
-        for num in nums:
-            if num in result:
-                stack.append("_")
-            else:
-                result.append(num)
-                length += 1
-        result.sort()
-        result += stack
-        return length, result
-
-    def removeDuplicates2(self, nums: list[int]) -> int:
         length = 0
         if len(nums) == 0:
             return length
-        for i in range(1,len(nums)):
+        for i in range(1, len(nums)):
             if nums[length] < nums[i]:
                 length += 1
                 nums[length] = nums[i]
         return length + 1
 
- 
-nums = [0,0,1,1,1,2,2,3,3,4]
-test = Solution().removeDuplicates2(nums)
-print(test, nums)
+    def removeDuplicates2(self, nums: list[int]) -> int:
+        # Two Pointers
+        i = 0
+        j = 0
+        while j < len(nums):
+            if nums[j] != nums[i]:
+                i += 1
+                nums[i] = nums[j]
+            j += 1
+        return i + 1
+
+    def removeDuplicates3(self, nums: list[int]) -> int:
+        # Two Pointers
+        slow, fast = 0, 1
+        
+        if len(nums) == 0 or len(nums) ==1:
+            return len(nums)
+        
+        while fast < len(nums):      
+		# fast pointer found distinct element so increment slow pointer and assign value which is at fast pointer.
+            if nums[slow] < nums[fast]:
+                slow += 1
+                nums[slow]=nums[fast]
+            fast += 1
+        return slow+1 
+                
+
+
+nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+nums.remove(nums[6])
+print(nums)
---
+++ LeetCode/0035_SearchInsertPosition.py
@@ -0,0 +1,12 @@
+# Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+class Solution:
+    def searchInsert(self, nums: list[int], target: int) -> int:
+        if target in nums:
+            return nums.index(target)
+        else:
+            nums.append(target)
+            nums.sort()
+            return nums.index(target)
