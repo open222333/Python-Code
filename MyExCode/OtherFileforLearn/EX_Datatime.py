1fcaa2880eab14305d2a0cb12d383da62c10d8e7
TomLi <open222333@@gmail.com>
2021-08-19 14:14:26 +0800

    20210819-021423

--- MyExCode/OtherFileforLearn/EX_Datatime.py
+++ MyExCode/OtherFileforLearn/EX_Datatime.py
@@ -5,3 +5,8 @@ from datetime import datetime
 
 # 如果輸入的值不是一個合法的時間，將觸發OverflowError 或ValueError。
 print(time.mktime(datetime.today().timetuple()))
+
+# 格式
+datati = '2021-08-13 17:00:02'
+print(datetime.strptime(datati, '%Y-%m-%d %H:%M:%S'))
+print(datetime.now().__format__('%Y-%m-%d %H:%M:%S'))
