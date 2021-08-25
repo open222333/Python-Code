10073481c1f2c968dba42a47d1baecb24210c294
TomLi <open222333@gmail.com>
2021-08-25 12:12:53 +0800

    no message

--- MyExCode/OtherFileforLearn/EX_request.py
+++ MyExCode/OtherFileforLearn/EX_request.py
@@ -1,4 +1,12 @@
 import requests
 
-url = 'https://ccl.jkimg02.xyz/imgs/actor/a3/54aaa99092bb0.jpg'
-print(requests.get(url).status_code, type(requests.get(url).status_code))
+# url = 'https://ccl.jkimg02.xyz/imgs/actor/a3/54aaa99092bb0.jpg'
+# print(requests.get(url).status_code, type(requests.get(url).status_code))
+
+
+def get_status_code(img_path) -> int:
+    return requests.get(img_path).status_code
+
+
+url = ''
+print(get_status_code(url))
