bedb479136d69f6ee0174d9229f3eded1266e561
TomLi <open222333@gmail.com>
2021-08-27 12:14:11 +0800

    Tool_def 新增 log , requests 新增重定向

--- MyExCode/OtherFileforLearn/EX_request.py
+++ MyExCode/OtherFileforLearn/EX_request.py
@@ -3,10 +3,21 @@ import requests
 # url = 'https://ccl.jkimg02.xyz/imgs/actor/a3/54aaa99092bb0.jpg'
 # print(requests.get(url).status_code, type(requests.get(url).status_code))
 
+# 重定向
+# https://www.cnblogs.com/xswt/p/11475164.html
 
-def get_status_code(img_path) -> int:
-    return requests.get(img_path).status_code
+
+# def get_status_code(img_path) -> int:
+#     return requests.get(img_path).status_code
 
 
-url = ''
-print(get_status_code(url))
+# url = ''
+# print(get_status_code(url))
+
+
+url = 'http://dekm6v70x775h.cloudfront.net/JKSR-235/240/JKSR-235.m3u8'
+r = requests.get(url, headers={"Content-Type": "application/json"})
+reditList = r.history
+print(f'獲取追蹤歷史：{r}')
+# print(f'headers：{reditList[0].headers}')
+# print(f'url：{reditList[len(reditList)-1].headers["location"]}')
--- MyExCode/OtherFileforLearn/Tool_def.py
+++ MyExCode/OtherFileforLearn/Tool_def.py
@@ -4,3 +4,14 @@ def get_status_code(img_path) -> int:
     '''測試鏈結 回傳http code'''
     import requests
     return requests.get(img_path).status_code
+
+
+def write_logs(file: str, message: str) -> None:
+    '''建立log'''
+    import os
+    if os.path.exists(file) == False:
+        f = open(file, 'w')
+        f.close()
+    f = open(file, 'a')
+    f.write(message + "\n")
+    f.close()
