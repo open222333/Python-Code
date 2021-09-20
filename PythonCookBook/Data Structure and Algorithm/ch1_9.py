dd072de60093b97dbfb925adddee35280f75d91d
TomLi <open222333@gmail.com>
2021-09-20 08:46:37 +0800

    20210920-084600
    python_cook_book

--- PythonCookBook/Data Structure and Algorithm/ch1_9.py
+++ PythonCookBook/Data Structure and Algorithm/ch1_9.py
@@ -1,55 +1,35 @@
 '''
-1.8 以字典進行運算
+1.9 找出兩個字典的共通處
 問題：
-在一個資料字典(dictionary)，進行各種運算。
+找出兩個字典共通的鍵值(keys)、值(values)
 解法：
-使用zip()反轉(invert)這個鍵值的keys與values。
-使用zip()搭配sorted()進行排序。
-注意：zip()建立一個只能消耗一次的迭代器(iterator)
+使用keys()或item()做簡單的集合運算(set operations)，這類運算也這類運算也可用來更動或過濾字典的內容。
 討論：
-若在字典進行常見的資料縮簡(data reductions)，只會處理鍵值(keys)。
-zip()反轉成(values,keys)的序列，先比較value，若剛好有一樣的值，會比較鍵值(key)。
+一個字典就是一個鍵值(key)集合與一個值(value)集合之間的映射(mapping)。
+字典的keys()會回傳一個揭露這些鍵值的keys-view物件。這些物件支援集合運算。
+字典的items()會回傳一個items-view物件，由(key, value)對組構成。這些物件支援集合運算。
+字典的values()不支援集合運算。原因：values-view 值不保證唯一，使得某些集合運算失去功用。
 '''
-prices = {
-    'ACME': 45.23,
-    'AAPL': 612.78,
-    'IBM': 205.55,
-    'HPQ': 37.20,
-    'FB': 10.75
+a = {
+    'x': 1,
+    'y': 2,
+    'z': 3
 }
 
-min_price = min(zip(prices.values(), prices.keys()))
-max_price = max(zip(prices.values(), prices.keys()))
+b = {
+    'w': 10,
+    'x': 11,
+    'y': 2
+}
 
-print(min_price)
-print(max_price)
+# 使用keys()或item()做簡單的集合運算(set operations)，這類運算也這類運算也可用來更動或過濾字典的內容。
+# 找出共通的鍵值(keys)
+print(a.keys() & b.keys())
+# 找出在a但不在b的鍵值
+print(a.keys() - b.keys())
+# 找出共通的對組
+print(a.items() & b.items())
 
-prices_sorted = sorted(zip(prices.values(), prices.keys()))
-print(prices_sorted)
-
-# 以下會出錯 注意：zip()建立一個只能消耗一次的迭代器(iterator)
-prices_and_names = zip(prices.values(), prices.keys())
-print(max(prices_and_names))  # OK
-try:
-    print(min(prices_and_names))  # ValueError: min() arg is an empty sequence
-except Exception as err:
-    print(err)
-
-# 若在字典進行常見的資料縮簡(data reductions)，只會處理鍵值(keys)。
-min(prices)
-max(prices)
-
-min(prices.values())
-max(prices.values())
-
-# 可回傳正確資料
-min(prices, key=lambda k: prices[k])
-max(prices, key=lambda k: prices[k])
-
-# 取得值須額外處理
-min_value = prices[min(prices, key=lambda k:prices[k])]
-
-# zip()反轉成(values,keys)的序列，先比較value，若剛好有一樣的值，會比較鍵值(key)。
-prices = {'AAA': 45.23, 'ZZZ': 45.23}
-print(min(zip(prices.values(), prices.keys())))
-print(max(zip(prices.values(), prices.keys())))
+# 製作一個特定鍵值被移除的新字典
+c = {key: a[key] for key in a.keys() - {'z', 'w'}}
+print(c)
