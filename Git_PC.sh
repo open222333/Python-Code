e0ddface56af79c4285d9e9aeb0ce1dd8cfec19b
open222333 <open222333@gmail.com>
2021-09-22 16:51:36 +0800

    20210922-165140
    git sh

--- Git_PC.sh
+++ Git_PC.sh
@@ -1,6 +1,6 @@
 time=$(date "+%Y%m%d-%H%M%S")
 ssh-add -K ~/.ssh/20210126_forGithub
-ssh-add -K ~/.ssh/20210526_id_ed25519_Email
+# ssh-add -K ~/.ssh/20210526_id_ed25519_Email
 pip freeze | tee requirements.txt
 git branch a
 git checkout a
@@ -8,6 +8,6 @@ git add *
 git commit -m"${time}"
 git checkout master
 git merge a
-#git pull 
+#git pull # 將上面的拉下來
 git push
 git branch -D a
