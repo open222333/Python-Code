f9aa44e4ff2a04df6cab2730c6164e6992cdf5b7
tom_li <tom_li@appdet.com>
2021-12-30 17:39:45 +0800

    mongo備份 設定文檔

--- MyExCode/Database_MongoDB/EX_mongodb_backup.py
+++ MyExCode/Database_MongoDB/EX_mongodb_backup.py
@@ -1,4 +1,5 @@
 import os
+from configparser import ConfigParser
 from datetime import datetime
 
 
@@ -10,13 +11,34 @@ def mongodump(host, db, col, target_dir='~/Desktop/'):
     if not os.path.exists(target_path):
         os.makedirs(target_path)
     command = f'mongodump -h {host} -d {db} -c {col} -o {target_path}'
+    print(command)
     os.system(command)
 
 
 def mongorestore(host, db, col, target_path):
     '''備份 mongo集合'''
     if not os.path.exists(target_path):
-        return False
+        print(f'{target_path} no exists')
     command = f'mongorestore -h {host} -d {db} -c {col} {target_path}/{db}/{col}.bson'
+    print(command)
     os.system(command)
-    return True
+
+
+os.chdir(os.path.dirname(__file__))
+config = ConfigParser()
+config.read('mongo_backup_config.ini', encoding='utf-8')
+
+host = config.get('MONGO', 'MONGO_HOST')
+db = config.get('MONGO', 'MONGO_DB')
+col = config.get('MONGO', 'MONGO_COLLECTION')
+target_dir = config.get('MONGO', 'TARGET_PATH')
+
+local_host = config.get('MONGO', 'MONGO_LOCAL_HOST')
+date = config.get('MONGO', 'DATE')
+target = f'{target_dir}mongo_backup_{col}/{date}'
+
+if bool(int(config.get('MONGO', 'MONGO_DUMP'))):
+    mongodump(host=host, db=db, col=col, target_dir=target_dir)
+
+if bool(int(config.get('MONGO', 'MONGO_RESTORE'))):
+    mongorestore(host=local_host, db=db, col=col, target_path=target, col_name=f'{col}_{date}')
