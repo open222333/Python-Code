34bd1a87e7e9590b347bfeda38e4b83a5b3d9934
Tom.Li <open222333@gmail.com>
2022-04-06 16:12:24 +0800

    pymongo re key_word search

--- MyExCode/Database_MongoDB/EX_mongodb_pymongo.py
+++ MyExCode/Database_MongoDB/EX_mongodb_pymongo.py
@@ -6,26 +6,7 @@ host = '127.0.0.1:31117'  # mongodb預設port
 client = MongoClient(host)
 db = client['test_db']['test_collection']
 
-# 查詢運算子範例
-filed = 'colimn'
-data = 'test'
 
-op_dict = {
-    'eq': {filed: {'$eq': data}},  # 等於
-    'ne': {filed: {'$ne': data}},  # 不等於
-    'lt': {filed: {'$lt': data}},  # 小於
-    'le': {filed: {'$lte': data}},  # 小於等於
-    'gt': {filed: {'$gt': data}},  # 大於
-    'ge': {filed: {'$gte': data}},  # 大於等於
-    'bw': {filed: {'$regex': f'^{data}'}},  # 開頭是
-    'bn': {filed: {'$not': {'$regex': f'^{data}'}}},  # 開頭不是
-    'in': {filed: {'$elemMatch': {'$eq': data}}},  # 在其中
-    'ni': {filed: {'$not': {'$elemMatch': {'$eq': data}}}},  # 不在其中
-    'ew': {filed: {'$regex': f'${data}'}},  # 結尾是
-    'en': {filed: {'$not': {'$regex': f'${data}'}}},  # 結尾不是
-    'cn': {filed: {'$in': [data]}},  # 內容包含(需用array)
-    'nc': {filed: {'$nin': [data]}},  # 內容不包含(需用array)
-}
 
 # 新增 insert
 data = {
@@ -41,10 +22,10 @@ datas = [{
     'name': f"test{i}",
     "createTime": datetime.now()
 } for i in range(1, 10)]
+
 db.insert_many(datas)
 
 # query查詢 EX_mongodb_pymongo_aggregate.py
-
 myclient = MongoClient("mongodb://localhost:27017/")
 mydb = myclient["runoobdb"]
 mycol = mydb["sites"]
@@ -63,6 +44,7 @@ myquery = {"name": {"$regex": "^F"}}
 newvalues = {"$set": {"alexa": "123"}}
 
 x = mycol.update_many(myquery, newvalues)
+
 # update 更新
 db.test_db.update_many()
 
