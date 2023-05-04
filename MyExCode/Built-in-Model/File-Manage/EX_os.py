477a2c90cc6526e22cad69a878f1e00c57e27173
tom_li <tom_li@appdet.com>
2023-05-04 17:11:14 +0800

    20230504

--- MyExCode/Built-in-Model/File-Manage/EX_os.py
+++ MyExCode/Built-in-Model/File-Manage/EX_os.py
@@ -221,3 +221,21 @@ def get_all_files(path: str, extensions: Union[int, float, None] = None, add_abs
 
     target_file_path.sort()
     return target_file_path
+
+
+def human_readable_size(size: int, decimal_places: int = 2) -> str:
+    """人類可讀 檔案大小
+
+    https://stackoverflow.com/questions/1094841/get-human-readable-version-of-file-size
+    Args:
+        size (int): 檔案大小 (bytes)
+        decimal_places (int, optional): 顯示小數位. Defaults to 2.
+
+    Returns:
+        str: 檔案大小 單位
+    """
+    for unit in ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB']:
+        if size < 1024.0 or unit == 'PiB':
+            break
+        size /= 1024.0
+    return f"{size:.{decimal_places}f} {unit}"
